52980272ebdaa4687cdc75a2e3c52e1af80724fa
kevin <kevinbrrs96@gmail.com>
2024-08-22 17:12:47 -0300

    Feat(Auth conectado a client a travez de nats)

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { LoginUserDto, RegisterUserDto } from './dto';
+
+@Controller()
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @MessagePattern('auth.register.user')
+  registerUser(@Payload() registeruserDto: RegisterUserDto) {
+    return registeruserDto;
+  }
+  @MessagePattern('auth.login.user')
+  loginUser(@Payload() loginUserDto: LoginUserDto) {
+    return loginUserDto;
+  }
+  @MessagePattern('auth.verify.user')
+  verifyToken() {
+    return 'verify USer';
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3004);
+  const logger = new Logger('Main-Auth-ms');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServers,
+      },
+    },
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen();
+  logger.log(`Auth Microservice running on PORT: ${envs.port}`);
 }
 bootstrap();
